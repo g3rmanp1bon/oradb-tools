476112e0a3bec457a71bf7be9eda889013338ad3
matut0 <german.x.pabon@gmail.com>
2022-05-06 09:09:48 -0400

    First version

---
+++ parser.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+var path = require('path');
+data = '';
+
+//Pass as argument the directory to analyze
+// node parser.js "/Users/germanpabon/Documents/customers/confuturo/statspack/Producción"
+
+const args = process.argv.slice(2);
+var directory = args[0];
+
+var server;
+var instance;
+
+// Lectura del directorio
+
+fs.readdir(directory, function (err, files) {
+    if (err) {
+        console.error("Could not list the directory.", err);
+        process.exit(1);
+    }
+
+    files.forEach(function (file, index) {
+        // Make one pass and make the file complete
+        var filePath = path.join(directory, file);
+
+        fs.stat(filePath, function (error, stat) {
+            if (error) {
+                console.error("Error stating file.", error);
+                return;
+            }
+
+            if (path.extname(filePath) == ".lst") {
+                //console.log(file);
+
+                try {
+                    data = fs.readFileSync(filePath, 'utf8');
+                } catch (err) {
+                    console.error(err);
+                }
+
+                // (physical read total bytes + physical write total bytes)/1024/1024 => IOMB/s
+
+                let PHYSICAL_READS_TOTAL_BYTES_REGXP = new RegExp(/(physical read total bytes[ #0-9,\.]{1,80})/);
+                let REGXP_VALUE = new RegExp(/^.{53}(.{14})/);
+
+                let PHYSICAL_WRITE_TOTAL_BYTES_REGXP = new RegExp(/(physical write total bytes[ #0-9,\.]{1,80})/);
+
+                var readMB = data.match(PHYSICAL_READS_TOTAL_BYTES_REGXP)[0].match(REGXP_VALUE)[0].replace(/,/g, '').slice(54, 68).trim();
+                var writeMB = data.match(PHYSICAL_WRITE_TOTAL_BYTES_REGXP)[0].match(REGXP_VALUE)[0].replace(/,/g, '').slice(54, 68).trim();
+
+                var IOMB = (parseFloat(readMB) + parseFloat(writeMB)) / 1024 / 1024;
+
+                // (physical write total IO requests + physical read total IO requests) = IOPS
+
+                let PHYSICAL_WRITE_TOTAL_IO_REGXP = new RegExp(/(physical write total IO requests[ #0-9,\.]{1,80})/)
+                let PHYSICAL_READ_TOTAL_IO_REGXP = new RegExp(/(physical read total IO requests[ #0-9,\.]{1,80})/)
+
+                var readIOPS = data.match(PHYSICAL_READ_TOTAL_IO_REGXP)[0].match(REGXP_VALUE)[0].replace(/,/g, '').slice(54, 68).trim();
+                var writeIOPS = data.match(PHYSICAL_WRITE_TOTAL_IO_REGXP)[0].match(REGXP_VALUE)[0].replace(/,/g, '').slice(54, 68).trim();
+
+                var IOPS = parseFloat(readIOPS) + parseFloat(writeIOPS);
+
+                server = file.split("_")[0];
+                instance = file.split("_")[1].split(".")[0];
+
+                let result = {
+                    server: server,
+                    instance: instance,
+                    IOMB: IOMB,
+                    IOPS: IOPS
+                }
+
+                console.log(result);
+            }
+
+        });
+    });
+});
